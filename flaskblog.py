b211467be70e7f3e01369df7c1c66e27f0217725
Samesh <samesh.lakhotia@gmail.com>
2019-03-21 03:25:40 +0530

    Add templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app=Flask(__name__)
+
+
+all_posts=[
+    {
+        'author':'Samesh',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'01/01/2019'
+    },
+    {
+        'author':'IDK',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'02/01/2019'
+
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Hello There!</h1>"
+    return render_template('home.html',posts=all_posts,title='Home')
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html',title='About Us')
 
 
 
