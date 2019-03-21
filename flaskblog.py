d7ff346a81173df78989a4fe2208d468ffb84e26
Samesh <samesh.lakhotia@gmail.com>
2019-03-21 11:25:20 +0530

    Add register and login forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,18 @@
-from flask import Flask,render_template,url_for
-app=Flask(__name__)
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 
+
+
+
+app=Flask(__name__)
+'''
+Generate hidden secert key with the help of pythons inbuilt secrets module.
+We are generating a random 16 bits key.
+
+import secerts
+secrets.token_hex(16)
+'''
+app.config['SECRET_KEY']='be2c117bdf7dfe089101bb27f4b99825'
 
 all_posts=[
     {
@@ -20,12 +32,43 @@ all_posts=[
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html',posts=all_posts,title='Home')
+
 
 @app.route("/about")
 def about():
     return render_template('about.html',title='About Us')
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+         flash(f'Account created for {form.username.data}!','success')
+         return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=='admin@blog.com' and form.password.data=='password':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful!','danger')
+    return render_template('login.html',title='Login',form=form)
+
+
+
+
+
+
+
+
+
 
 
 
