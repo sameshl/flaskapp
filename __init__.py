cc460226a4c9ab93d529537049d27c4ea1c11748
Samesh <samesh.lakhotia@gmail.com>
2019-03-21 18:06:44 +0530

    Add User Authentication system

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app=Flask(__name__)
 '''
@@ -15,6 +15,11 @@ secrets.token_hex(16)
 app.config['SECRET_KEY']='be2c117bdf7dfe089101bb27f4b99825'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login' #function name of route to be taken
+login_manager.login_message_category='info'
+
 
 
 from flaskblog import routes
