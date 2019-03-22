46e1664a4b6d97bff967b1ff3adebdb63f344ae4
Samesh <samesh.lakhotia@gmail.com>
2019-03-22 19:32:06 +0530

    Add small changes and comments

--- routes.py
+++ routes.py
@@ -73,35 +73,34 @@ def logout():
 
 
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+        random_hex=secrets.token_hex(8)#we are randomising the name of picture because 2 people can have the same file name uplaoded
+        _,f_ext=os.path.splitext(form_picture.filename)#get the extention(.png/.jpg) of the picture uploaded
+        picture_fn= random_hex+ f_ext
+        picture_path=os.path.join(app.root_path,'static/profile_pics',picture_fn)
 
-    output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-
-    return picture_fn
+        output_size=(125,125)
+        i=Image.open(form_picture)
+        i.thumbnail(output_size)
+        i.save(picture_path)
+        return picture_fn
 
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    form = UpdateAccountForm()
+    form=UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file=picture_file
+        current_user.username=form.username.data
+        current_user.email=form.email.data
         db.session.commit()
-        flash('Your account has been updated!', 'success')
+        flash('Your account has been updated!','success')
         return redirect(url_for('account'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account',
-image_file=image_file, form=form)
+    elif request.method=='GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email
+    image_file=url_for('static',filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html', title='Account'
+    ,image_file=image_file,form=form)
