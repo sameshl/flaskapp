c88ad455f09070bcb90a8ef062cd0b03d6d005dd
Samesh <samesh.lakhotia@gmail.com>
2019-03-23 11:02:33 +0530

    Add Pagination

--- routes.py
+++ routes.py
@@ -12,7 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts=Post.query.all()
+    page=request.args.get('page',1,type=int)
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(per_page=4,page=page)
     return render_template('home.html', posts=posts)
 
 
@@ -144,3 +145,14 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your Post has been deleted!",'success')
     return redirect(url_for('home'))
+
+
+
+@app.route("/user/<string:username>")
+def user_post(username):
+    page=request.args.get('page',1,type=int)
+    user=User.query.filter_by(username=username).first_or_404()
+    posts=Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(per_page=4,page=page)
+    return render_template('user_posts.html', posts=posts, user=user)
