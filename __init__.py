4a0325532a5f6abe58b0ad99080cfd60fc0e5c23
Samesh <samesh.lakhotia@gmail.com>
2019-03-23 17:05:16 +0530

    Add Password Reset System

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
+
+
 
 app=Flask(__name__)
 '''
@@ -19,6 +24,19 @@ bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view='login' #function name of route to be taken
 login_manager.login_message_category='info'
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+#app.config['MAIL_USERNAME']=os.environ.get('EMAIL_USER')
+#app.config['MAIL_PASSWORD']=os.environ.get('EMAIL_PASS')
+
+app.config['MAIL_USERNAME']='demouser967'
+app.config['MAIL_PASSWORD']='demopassword'
+
+
+mail=Mail(app)
+
 
 
 
